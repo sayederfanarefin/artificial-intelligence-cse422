237e1904cbf69fc2e9d223a7510524e5b2d9ff87
Sayed Erfan Arefin <erfanjordison@gmail.com>
2016-05-24 10:49:36 +0600

    blank

---
+++ simple_problem_solving_agent_gadhagasker/src/simple_problem_solving_agent_gadhagasker/Simple_problem_solving_agent_gadhagasker.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simple_problem_solving_agent_gadhagasker;
+
+/**
+ *
+ * @author erfan
+ */
+public class Simple_problem_solving_agent_gadhagasker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
